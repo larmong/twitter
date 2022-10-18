4419004784020143b21016c070fa8a4f6a1b28bf
larmong <larmong@naver.com>
2022-10-19 01:44:36 +0900

    [ADD] Firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASNR9_ABeLtCU0f6S3_d6MdNCUFZ-B6yA",
+  authDomain: "twitter-76f66.firebaseapp.com",
+  projectId: "twitter-76f66",
+  storageBucket: "twitter-76f66.appspot.com",
+  messagingSenderId: "89321770940",
+  appId: "1:89321770940:web:12ad20d164eadb4dc2a5b9"
+};
+
+export default initializeApp(firebaseConfig);
