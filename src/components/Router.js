d06046694e330dd5d1f25bfd6ecaf8fcb0f505cc
larmong <larmong@naver.com>
2022-10-20 21:20:34 +0900

    [FIX] Routes setting

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,20 +1,24 @@
-import React, {useState} from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
 const AppRouter = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <BrowserRouter>
-      {/*<Routes>*/}
-      {/*  {*/}
-      {/*    isLoggedIn ?*/}
-      {/*      <Route path="/home"><Home/></Route> :*/}
-      {/*      <Route path="/"><Auth/></Route>*/}
-      {/*  }*/}
-      {/*</Routes>*/}
-    </BrowserRouter>
+    <Router>
+      <Switch>
+        { isLoggedIn ? (
+            <>
+              <Route><Home /></Route>
+            </>
+            ) : (
+            <>
+              <Route><Auth /></Route>
+            </>
+          )}
+      </Switch>
+    </Router>
   )
 }
 
