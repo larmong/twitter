0712e069a69418933c1d205a91c6dbb27abaa665
larmong <larmong@naver.com>
2022-10-24 23:09:53 +0900

    [FIX] LogIn

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,7 +4,8 @@ import {authService} from "../fbase";
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(true)
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("")
   const onChange = (event) => {
     const {
       target: { name, value},
@@ -26,21 +27,26 @@ const Auth = () => {
       }
       console.log(data)
     } catch (error) {
-      console.log(error)
+      setError(error.message)
     }
-
   };
+  const toggleAccount = () => setNewAccount(prev => !prev);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} name="email" type="text" placeholder="이메일을 입력해주세요." required value={email}/>
         <input onChange={onChange} name="password" type="password" placeholder="비밀번호를 입력해주세요." required value={password}/>
         <input type="submit" value={ newAccount ? "Create Account" : "Login" }/>
+        {error}
         <div>
           <button>Google 로그인</button>
           <button>Github 로그인</button>
         </div>
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "SignIn" : "Create Account"}
+      </span>
     </div>
   )
 }
